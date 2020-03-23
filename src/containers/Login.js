3f08def2c37a3ef7620c65eb16c854eaa20547a4
Alicia Chao <aliciachao@gmail.com>
2020-03-23 00:49:44 -0700

    login container

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Grid from '@material-ui/core/Grid';
+import { api } from '../services/api';
 
 class Login extends Component {
 
@@ -9,30 +10,46 @@ class Login extends Component {
     }
 
     handleChange = (e) => {
-        this.setState({
-            username: e.target.value,
-            password: e.target.value
-        })
+        this.setState(
+            {[e.target.name]: e.target.value}
+        )
     }
 
     handleSubmit = (e) => {
         e.preventDefault();
-        this.props.logUserIn(this.state.username)
+        api.auth.login(this.state).then(res=>{
+            if(!res.error) {
+                this.props.onLogin(res);
+                this.props.history.push('/')
+            } else {
+                this.setState({ error: true });
+            }
+        })
+        // this.props.loginUser(this.state.username, this.state.password)
+        // this.setState({username: "", password: ""}) //reset form 
     }
 
     render() {
-        return(
+        return this.props.loggedIn? (
+            <Redirect to="/profile"/>
+            ) : ( 
             <Grid container justify="center" >
-                <div className="Login">
+              <div className="login form">
                 <form onSubmit={this.handleSubmit}>
-                    <label><h2>LOGIN</h2></label>
-                    <input type="text" placeholder="Enter Username" name="uname" onChange={(e) => this.handleChange(e)}/>
-                    <input type="text" placeholder="Enter Password" name="pw" onChange={(e) => this.handleChange(e)}/>
+                    {/* <Message error header={this.props.failedLogin? this.props.error : null}/> */}
+                    <label>Username</label>
+                    <input 
+                        type="text" placeholder="Enter Username" name="username" 
+                        onChange={this.handleChange} value={this.state.username}/>
+                    <label>Password</label>
+                    <input 
+                        type="password" placeholder="Enter Password" name="password" 
+                        onChange={this.handleChange} value={this.state.password}/>
                     <button type="submit">Login</button>
-                </form>
-                </div>
-            </Grid>
-        )
+                </form>  
+              </div>
+            </Grid> 
+            )
     }
 }
 
