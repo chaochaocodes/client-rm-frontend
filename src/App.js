f5c1e236179e0d0032bc604b660f3adff2dedae8
Alicia Chao <aliciachao@gmail.com>
2020-03-25 19:30:21 -0700

    working on search function, 500 error

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './css/App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import IndexContainer from './containers/IndexContainer';
+import SearchContainer from './containers/SearchContainer';
 import NavBar from './components/NavBar'
 import SignUp from './components/SignUp';
 import Login from './components/Login';
@@ -17,9 +18,9 @@ import { api } from "./services/api";
 
 class App extends Component {
   state = {
+    loggedIn: false,
     auth: {
       user: {}
-      // logged in boolean
     }
   }
 
@@ -28,43 +29,38 @@ class App extends Component {
     if(token) {
       api.auth.getCurrentUser().then(user => {
         const updatedState = { ...this.state.auth, user: user };
-        this.setState({auth: updatedState})
+        this.setState({loggedIn: true, auth: updatedState})
       })
     }
   }
-
-//  SAMPLE REQUEST
-//   fetch('http://localhost:3000/api/v1/Dashboard', {
-//   method: 'GET',
-//   headers: {
-//     Authorization: `Bearer <token>`
-//   }
-// })
 
   signup = data => {
     console.log("Signup", data )
     const updatedState = {...this.state.auth, 
       user: {id: data.id, username: data.username}};
     localStorage.setItem("token", data.jwt);
-    this.setState({auth: updatedState});
+    this.setState({loggedIn: true, auth: updatedState});
   }
 
   login = data => {
     const updatedState = {...this.state.auth, 
       user: {id: data.id, username: data.username}};
     localStorage.setItem("token", data.jwt);
-    this.setState({auth: updatedState});
+    this.setState({loggedIn: true, auth: updatedState});
   };
   
   logout = () => {
     console.log("token", "logging out!")
     localStorage.removeItem("token");
-    this.setState({ auth: { user: {} } })
+    this.setState({ loggedIn: false, auth: { user: {} } })
   }
 
   handleDelete = () => {
+    console.log(this.state.auth.user, "Handle delete")
     console.log( "app.js handle delete")
-    api.auth.deleteAccount();
+    api.auth.deleteAccount(this.state.auth.user)
+    .then(this.logout());
+    // this.logout();
 }
 
   render() {
@@ -79,10 +75,11 @@ class App extends Component {
             <Switch>
                 <Route exact path="/"><Home/></Route>
                 <Route path="/index" component={IndexContainer} /> 
+                <Route path="/search" component={SearchContainer} /> 
                 <Route path="/signup" render={props => <SignUp {...props} onSignup={this.signup}/>}></Route>  
                 <Route path="/login" render={props => <Login {...props} onLogin={this.login}/>}></Route>  
                 <Route path="/logout" ><Logout/></Route>  
-                <Route path="/account" render={props => <Account {...props} onDelete={this.handleDelete}/>}></Route> 
+                <Route path="/account" render={props => <Account {...props} onDelete={this.handleDelete} currentUser={this.state.auth.user} />}></Route> 
                 <Route path="/dashboard"><Dashboard/></Route>
                 <Route path="/delete"><Delete/></Route>
                 <Route path="/tracker"><Tracker/></Route>
