811943373476e849f6e0e045bc31574c9d9514c7
Alicia Chao <aliciachao@gmail.com>
2020-03-26 20:06:01 -0700

    navbar: loggedin? button access

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,21 +12,35 @@ function handleLogout() {
   props.onLogout();
 }
 
+const loggedinBtns = ()=> {
+  return (
+    <div>
+    <Button color="inherit" component={Link} to="/account">Account</Button>
+    <Button color="inherit" component={Link} to="/tracker">Tracker</Button>
+    <Button color="inherit" component={Link} to="/dashboard">Dashboard</Button>
+    <Button color="inherit" onClick={handleLogout} component={Link} to="/logout">Logout</Button>
+    </div>
+  )
+}
+
+const loggedoutBtns = () => {
+  return(
+    <div>
+      <Button color="inherit" component={Link} to="/signup">Sign Up</Button>
+      <Button color="inherit" component={Link} to="/login">Login</Button>
+      {/* <Button for="radioHL" component={Link} to="/">{props.loggedIn ? "Home" : "Login"}</Button> */}
+    </div>
+  )
+}
+
   return(
         <div>
           <AppBar position="fixed" color="primary">
             <Toolbar>
-              {/* <Typography variant="h6">Client-RM</Typography> */}
               <Button size="large" color="inherit" component={Link} to="/">Client-RM</Button>
-              {/* <Button for="radioHL" component={Link} to="/">{props.loggedIn ? "Home" : "Login"}</Button> */}
               <Button color="inherit" component={Link} to="/about">About</Button>
               <Button color="inherit" component={Link} to="/index">Listings</Button>
-              <Button color="inherit" component={Link} to="/account">Account</Button>
-              <Button color="inherit" component={Link} to="/tracker">Tracker</Button>
-              <Button color="inherit" component={Link} to="/dashboard">Dashboard</Button>
-              <Button color="inherit" onClick={handleLogout} component={Link} to="/logout">Logout</Button>
-              <Button color="inherit" component={Link} to="/signup">Sign Up</Button>
-              <Button color="inherit" component={Link} to="/login">Login</Button>
+              { props.loggedIn ? loggedinBtns() : loggedoutBtns() }
             </Toolbar>
           </AppBar>
         </div>
