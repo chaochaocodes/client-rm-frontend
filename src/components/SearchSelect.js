1793ea38e21b9739f47f94ed5c556e7ccfd31c78
github username <github email address>
2020-04-18 00:26:12 -0700

    SearchSelect wip

--- src/components/SearchSelect.js
+++ src/components/SearchSelect.js
@@ -4,8 +4,8 @@ import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import NativeSelect from '@material-ui/core/NativeSelect';
 import InputBase from '@material-ui/core/InputBase';
-// import {Button} from '@material-ui/core/'
-// import {Link} from 'react-router-dom'
+import {Button} from '@material-ui/core/'
+import {Link} from 'react-router-dom'
 // import { api } from "../services/api";
 
 const BootstrapInput = withStyles(theme => ({
@@ -52,47 +52,49 @@ const useStyles = makeStyles(theme => ({
 
 const SearchSelect = (props) => {
   const classes = useStyles();
-  // const [age, setCity] = React.useState('');
+  const [state, setState] = React.useState('');
+  
   const handleChange = event => {
-    // setCity(event.target.value);
+    setState(event.target.value);
+    // pass city value into Search Container..
   };
+
+  const stateArray = [
+		"AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC",  
+    "DE", "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA",  
+    "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE",  
+    "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC",  
+    "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY"]
 
   return (
     <div>
      <h3>Enter City, State to Start Exploring!</h3>
-      <form onSubmit={ e => props.handleSubmit(e) }>
+      {/* <form onSubmit={ e => props.handleSubmit(e) }>
         <input name="city" label="city" placeholder="Enter City"/>
         <input name="state" label="state" placeholder="Enter State"/>
         <button type="submit"> Search </button>
-        {/* <Button type="submit" variant="outlined" component={Link} to="/search">Search</Button> */}
-      </form>
+      </form> */}
 
+      <form onSubmit={ e => props.handleSubmit(e)} noValidate autoComplete="off">
       <FormControl className={classes.margin}  >
-        <InputLabel htmlFor="demo-customized-textbox" >City</InputLabel>
-        <BootstrapInput id="demo-customized-textbox" name="city" label="city" placeholder="Enter City"/>
+        <InputLabel htmlFor="input-city" >City</InputLabel>
+        <BootstrapInput id="city" name="city" label="city"/>
       </FormControl>
 
       <FormControl className={classes.margin}>
-        <InputLabel htmlFor="demo-customized-textbox" >State</InputLabel>
-        <BootstrapInput id="demo-customized-textbox" name="state" label="state" placeholder="Enter State"/>
-      </FormControl>
-
-      <FormControl className={classes.margin}>
-        <InputLabel htmlFor="demo-customized-select-native">City</InputLabel>
+        <InputLabel htmlFor="select-state">State</InputLabel>
         <NativeSelect
-          id="demo-customized-select-native"
-          // value={city}
+          id="select-state"
+          value={state}
           onChange={handleChange}
           input={<BootstrapInput />}
         >
-          <option aria-label="None" value="" label="Search by City" />
-          <option value={10}>New York City, NY</option>
-          <option value={20}>San Francisco, CA</option>
-          <option value={30}>Seattle, WA</option>
-          <option value={30}>Denver, CO</option>
-          <option value={30}>Chicago, IL</option>
+          <option aria-label="None" value="" label=" " />
+          {stateArray.map(state => <option value={state}>{state}</option> )}
         </NativeSelect>
       </FormControl>
+      <Button className={classes.margin}  type="submit" variant="outlined" component={Link} to="/search">Search</Button>
+      </form>
     </div>
   );
 }
